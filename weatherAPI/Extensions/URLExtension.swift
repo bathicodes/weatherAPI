ff59af8b3b5cccd524796285239470c21746b60c
Bathiya Seneviratne <seneviratne.bathiya@gmail.com>
2020-12-04 18:03:23 +0530

    New changes

--- weatherAPI/Extensions/URLExtension.swift
+++ weatherAPI/Extensions/URLExtension.swift
@@ -9,7 +9,7 @@ import Foundation
 
 extension URL {
     static func weatherURL() -> URL? {
-        guard let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=London&appid=a0805d388a39e56abde9ae0adcac2106") else {
+        guard let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=London&appid=12121121211212111211212") else {
             return nil
         }
         return url
